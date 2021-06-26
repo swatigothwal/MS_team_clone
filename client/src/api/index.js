28b8bb22fd95c5ae553b4f5522e4c036d25bd125
swatigothwal <swatigothwal57@gmail.com>
2021-06-27 01:29:23 +0530

    4deploy

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001/",
+  baseURL: process.env.REACT_APP_HOST_URL,
   headers: { "Content-Type": "application/json; charset=UTF-8" },
 });
 
